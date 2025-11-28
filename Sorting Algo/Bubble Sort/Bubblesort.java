63df731b39363b9875b925654d58fa60f6333ed1
SVVV Campus Scout <campusscout@svvv.edu.in>
2025-11-29 00:24:28 +0530

    sort: bubble sort

---
+++ Sorting Algo/Bubble Sort/Bubblesort.java
@@ -0,0 +1,28 @@
+public class Bubblesort {
+    public static void swap(int arr[],int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void sort(int arr[]){
+        for (int i = 0; i <= arr.length-1; i++) {
+            for (int j = 0; j <= arr.length-2; j++) {
+                if (arr[j] > arr[j+1]) {
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int nums[] = {1,5,3,7,4,2};
+        System.out.println("Before Swapping: ");
+        for (int i : nums) {
+            System.out.print(i);
+        }
+        sort(nums);
+        System.out.println("\nAfter Swapping: ");
+        for (int i : nums) {
+            System.out.print(i);
+        }
+    }   
+}
