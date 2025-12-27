76ed792c23374206c19b1f0fd4233cc245889258
jatinrane14 <jatinrane23@gmail.com>
2025-12-27 21:37:22 +0530

    Arrays: Is array is sorted

---
+++ Arrays/4. is Array is sorted/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static Boolean isSorted(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            if (!(arr[i] <= arr[i+1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,7,5,6};
+        System.out.println(isSorted(arr));
+    }
+}
