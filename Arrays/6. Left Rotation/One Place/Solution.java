6742b9cc85e8a0c42d9bf0e4a77cc3217c7e47ee
jatinrane14 <jatinrane23@gmail.com>
2026-01-09 01:47:44 +0530

    Array Right rotation by one place

--- Arrays/6. Left Rotation/One Place/Solution.java
+++ Arrays/6. Left Rotation/One Place/Solution.java
@@ -1,7 +1,15 @@
 import java.util.Arrays;
 
 public class Solution{
-    public static void rotate(int arr[]){
+    public static void rotateRight(int arr[]){
+        int n = arr.length;
+        int temp = arr[n-1];
+        for (int i = n-2; i >=0; i--) {
+            arr[i+1] = arr[i];
+        }
+        arr[0] =temp;
+    }
+    public static void rotateLeft(int arr[]){
         int temp = arr[0];
         for (int i = 1; i < arr.length; i++) {
             arr[i-1] = arr[i];
@@ -10,9 +18,10 @@ public class Solution{
     }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6};
-        System.out.println("Before Rotating by one place");
+        System.out.println("Before Left/Right Rotating by one place");
         System.out.println(Arrays.toString(arr));
-        rotate(arr);
+        // rotateLeft(arr);
+        rotateRight(arr);
         System.out.println("After Rotating by one place");
         System.out.println(Arrays.toString(arr));
     }
