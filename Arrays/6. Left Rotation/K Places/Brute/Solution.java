6110f8a03baed0672feaf4d8f3c7d26284a6fbb8
jatinrane14 <jatinrane23@gmail.com>
2026-01-09 02:05:15 +0530

    Arrays: Right rotate by k places

--- Arrays/6. Left Rotation/K Places/Brute/Solution.java
+++ Arrays/6. Left Rotation/K Places/Brute/Solution.java
@@ -1,8 +1,27 @@
-package Brute;
 import java.util.Arrays;
 
 public class Solution {
-    public static void rotateK(int[] arr,int k){
+    public static void rotateRight(int arr[], int k){
+        int n = arr.length;
+        if (n==0) return;
+
+        k=k%n;
+
+        int[] temp =  new int[k];
+        int in =0;
+        for (int i = n-k; i <n; i++) {
+            temp[in] = arr[i];
+            in++;
+        }
+        for (int i = n-k-1; i >=0; i--) {
+            arr[i+k] = arr[i];
+        }
+
+        for(int i =0;i<k;i++){
+            arr[i] = temp[i];
+        }
+    }
+    public static void rotateKLeft(int[] arr,int k){
         int n = arr.length;
         if (n==0) {
             return;
@@ -25,7 +44,8 @@ public class Solution {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
         System.out.println(Arrays.toString(arr));
-        rotateK(arr, 5);
+        // rotateKLeft(arr, 3);
+        rotateRight(arr, 3);
         System.out.println(Arrays.toString(arr));
     }
 }
