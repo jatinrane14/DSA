f6fb0f948844dd48e856b3d6784a32e1441382a9
jatinrane14 <jatinrane23@gmail.com>
2026-01-13 21:58:50 +0530

    Array : Missing number Optimal Approach 2

--- Arrays/11. Missing number/Optimal/Solution2.java
+++ Arrays/11. Missing number/Optimal/Solution2.java
@@ -6,14 +6,11 @@ public class Solution2 {
             XOR1 ^=arr[i];
             XOR2 ^=(i+1);
         }
-        System.out.println("Withour N= "+XOR1);
         XOR1 ^= arr.length+1;
-        System.out.println("With N= "+XOR1);
-        System.out.println(XOR2);
         return XOR1^XOR2;
     }
     public static void main(String[] args) {
         int arr[] = {1,3,4,5,6};
-        System.out.println(findMissing(arr));
+        System.out.println("Missing Number is: "+findMissing(arr));
     }
 }
