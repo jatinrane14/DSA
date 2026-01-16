6eaa5f610fd48e6e29291c0f8dcfa662cc8a8e2c
jatinrane14 <jatinrane23@gmail.com>
2026-01-16 22:06:35 +0530

    Arrays: Longest Subarray with k sum optimizing brute solution

---
+++ Arrays/Longest Subarray with sum K/Brute/Solution2.java
@@ -0,0 +1,19 @@
+public class Solution2 {
+    public static int getSubarraySum(int arr[],int target){
+        int maxSize= 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum=0;
+            for (int k = i; k < arr.length; k++) {
+                sum+=arr[k];
+                if(sum==target){
+                    maxSize = Math.max(maxSize,k-i+1);
+                }
+            }
+        }
+        return maxSize;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,1,1,1,1,4,2,3};
+        System.out.println(getSubarraySum(arr, 9));
+    }    
+}
