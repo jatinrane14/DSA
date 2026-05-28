796bb12a35f614ad82f1442651bc06e9ce285c73
jatinrane14 <jatinrane23@gmail.com>
2026-05-28 06:06:56 +0530

    Slightly optimize remove duplicates solution

--- Arrays/5. Remove duplicates/Better Soln/Solution.java
+++ Arrays/5. Remove duplicates/Better Soln/Solution.java
@@ -3,8 +3,8 @@ public class Solution {
         int i =0;
         for (int j = 1; j < arr.length; j++) {
             if (arr[j] != arr[i]) {
-                arr[i+1] = arr[j];
                 i++;
+                if(i!=j) arr[i] = arr[j];
             }
         }
         return i+1;
